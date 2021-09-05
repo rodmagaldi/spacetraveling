a243b4c081f8b32d170e791d392230828313d1b1
Rodrigo Magaldi <rod.magaldi@gmail.com>
2021-09-05 19:58:28 +0200

    Fetch more posts on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { FiCalendar } from 'react-icons/fi';
 import { FiUser } from 'react-icons/fi';
-import logoImg from '../../public/images/logo.svg';
 
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
+
+import logoImg from '../../public/images/logo.svg';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -30,7 +37,58 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const fetchPosts = async (page = 1) => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+      pageSize: 1,
+      page,
+    }
+  );
+
+  return response;
+};
+
+const mapPostsPagination = (response: any): PostPagination => {
+  const postsPagination: PostPagination = {
+    next_page: response.next_page,
+    results: response.results.map(post => ({
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd LLL yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        author: post.data.author,
+        subtitle: post.data.subtitle,
+        title: post.data.title,
+      },
+    })),
+  };
+
+  return postsPagination;
+};
+
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [page, setPage] = useState(1);
+  const [posts, setPosts] = useState(postsPagination);
+
+  const handleLoadMore = async (): Promise<void> => {
+    setPage(page + 1);
+    const newPosts = mapPostsPagination(await fetchPosts(page + 1));
+    setPosts({
+      next_page: newPosts.next_page,
+      results: [...posts.results, ...newPosts.results],
+    });
+  };
+
   return (
     <>
       <Head>
@@ -42,76 +100,48 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
         </div>
 
         <section className={styles.content}>
-          <article>
-            <h1>Como utilizar Hooks</h1>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div className={styles.postInfo}>
-              <div className={styles.postInfoSection}>
-                <FiCalendar />
-                <time className={styles.iconText}>15 Mar 2021</time>
+          {posts?.results?.map(post => (
+            <article key={post.uid}>
+              <h1>{post.data.title}</h1>
+              <p>{post.data.subtitle}</p>
+              <div className={styles.postInfo}>
+                <div className={styles.postInfoSection}>
+                  <FiCalendar />
+                  <time className={styles.iconText}>
+                    {post.first_publication_date}
+                  </time>
+                </div>
+                <div className={styles.postInfoSection}>
+                  <FiUser />
+                  <span className={styles.iconText}>{post.data.author}</span>
+                </div>
               </div>
-              <div className={styles.postInfoSection}>
-                <FiUser />
-                <span className={styles.iconText}>Diego Fernandes</span>
-              </div>
-            </div>
-          </article>
+            </article>
+          ))}
 
-          <article>
-            <h1>Como utilizar Hooks</h1>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div className={styles.postInfo}>
-              <div className={styles.postInfoSection}>
-                <FiCalendar />
-                <time className={styles.iconText}>15 Mar 2021</time>
-              </div>
-              <div className={styles.postInfoSection}>
-                <FiUser />
-                <span className={styles.iconText}>Diego Fernandes</span>
-              </div>
-            </div>
-          </article>
-
-          <article>
-            <h1>Como utilizar Hooks</h1>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div className={styles.postInfo}>
-              <div className={styles.postInfoSection}>
-                <FiCalendar />
-                <time className={styles.iconText}>15 Mar 2021</time>
-              </div>
-              <div className={styles.postInfoSection}>
-                <FiUser />
-                <span className={styles.iconText}>Diego Fernandes</span>
-              </div>
-            </div>
-          </article>
-
-          <article>
-            <h1>Como utilizar Hooks</h1>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div className={styles.postInfo}>
-              <div className={styles.postInfoSection}>
-                <FiCalendar />
-                <time className={styles.iconText}>15 Mar 2021</time>
-              </div>
-              <div className={styles.postInfoSection}>
-                <FiUser />
-                <span className={styles.iconText}>Diego Fernandes</span>
-              </div>
-            </div>
-          </article>
-
-          <span className={styles.loadMore}>Carregar mais posts</span>
+          {posts.next_page && (
+            <span
+              className={styles.loadMore}
+              role="button"
+              tabIndex={0}
+              onClick={handleLoadMore}
+              onKeyDown={handleLoadMore}
+            >
+              Carregar mais posts
+            </span>
+          )}
         </section>
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetchPosts();
 
-//   // TODO
-// };
+  const postsPagination = mapPostsPagination(response);
+
+  return {
+    props: { postsPagination },
+  };
+};
